a9db0aacee9b642cb97d0424bc3b143dc0d3dc55
Arkxd <74634345+Arkxd@users.noreply.github.com>
2023-05-23 23:03:56 +0800

    Initial commit

---
+++ src/BookDB/DBBean.java
@@ -0,0 +1,6 @@
+package BookDB;
+
+public class DBBean {
+    private String driverStr = "com.mysql.jc.jdbc.Driver";
+    private String connStr = "jdbc:mysql://localhost:3306/book?characterEncoding="
+}
